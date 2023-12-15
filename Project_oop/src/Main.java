47e91dbb8cdfa471c805744d0e90f687fe4aafd9
Aryan Chaudhary <107054263+AryanCdry@users.noreply.github.com>
2023-12-15 22:11:08 +0545

    Update Main.java

--- Project_oop/src/Main.java
+++ Project_oop/src/Main.java
@@ -13,10 +13,27 @@ public class Main {
         System.out.println("Marks: " + student.getMarks() );
         System.out.println("Percentage: " + student.getPercentage() + "%");
 
-    
+        Students student2 = new Students("Ayush", "Yala", 24065 , true, marks2);
+        System.out.println("Student Name: " + student2.getName());
+        System.out.println("Section: " + student2.getSection());
+        System.out.println("Roll No.: " + student2.getRoll_no());
+        System.out.println("Marks: " + student2.getMarks() );
+        System.out.println("Percentage: " + student2.getPercentage() + "%");
+
+        Students student3 = new Students("XYZ", "Yala", 24066 , true, marks1);
+        System.out.println("Student Name: " + student3.getName());
+        System.out.println("Section: " + student3.getSection());
+        System.out.println("Roll No.: " + student3.getRoll_no());
+        System.out.println("Marks: " + student3.getMarks() );
+        System.out.println("Percentage: " + student3.getPercentage() + "%");
                 student.displayInformation();
             }
         }
+
+
+
+
+
 
 
 
